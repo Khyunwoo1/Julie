e405e4dc78149cc985bf0182d09b94398053c7d0
Paul Kim <khyunwoo1@gmail.com>
2021-03-21 17:39:19 -0400

    beginnings of ranking algo, still need to figure out XPATH

--- Julie Chrome Extension/content.js
+++ Julie Chrome Extension/content.js
@@ -1,13 +1,61 @@
-// var nodes = document.evaluate("//body[@id='walking-icon']", document, null, XPathResult.ANY_TYPE, null)
-// var resultNode = nodes.iterateNext()
-// if (resultNode) {
+function domTraversal(){
 
-//   // Found the first node. Output its contents.
-//   // basically outputs everything contained within the thing
-//   // returns string
-//   // can regex
-//   console.log(resultNode.innerHTML);
-// }
+  const arrOfImportantEls = [];
+  
+  // tags obj should probably be universal
+  const tags = {
+    a: 'a',
+    button: 'button',
+  }
+  
+  // and the unique ones will be brought to front end from backend
+  const dominosHomePageParams = {
+    order: 'order online',
+    track: 'track order',
+    delivery: 'delivery'
+  }
+
+  const w3SchoolsHomePageParams = {
+    learnSQL: 'LEARN SQL',
+    SQLReference: 'SQL REFERENCE',
+    learnPython: 'LEARN PYTHON',
+  }
+  
+  // Since dominos dom is protected: loop through obj of w3SchoolsHomePageParams
+  // basically checks based off "type" of website, what Julie believes would be important elements
+  for(let key in w3SchoolsHomePageParams){
+    
+    const importantEl = {};
+    const currentParam = w3SchoolsHomePageParams[key];
+    // will regex ^ this string to be more dynamic
+
+
+    // XPATH is async, document.getlementby is not.
+    alert(document.getElementsByClassName("w3-button w3-dark-grey"))
+    // do xpath search of each critera
+    // search param is for "a", but need to make dynamic also
+    // CASE SENSITIVE!!!
+    const nodes = document.evaluate(`//a[text()="${currentParam}"]`, document, null, XPathResult.ANY_TYPE, null);
+    const resultNode = nodes.iterateNext();
+    // should put into obj with element type and any relevant info like inner html or class/id name
+    // push results into arr of important els
+
+    if(resultNode){
+      // Need a way to get the current result node's el/tag type and innerHTML
+      const nodeText = resultNode.innerHTML;
+      alert('inside result node if ')
+      importantEl.name = nodeText;
+      // importantEl[dom_element] = 
+      arrOfImportantEls.push(importantEl)
+    } else {
+      alert('inside else of result node')
+    }
+  }
+  // returns out the array that'll be fetched
+  return arrOfImportantEls;
+}
+
+
 
 
 setTimeout(function(){ 
@@ -16,25 +64,9 @@ setTimeout(function(){
   function julieTalks(message){
     let msg = new SpeechSynthesisUtterance(message)
       let voices = window.speechSynthesis.getVoices()
-      msg.voice = voices[3]
+      msg.voice = voices[4]
       window.speechSynthesis.speak(msg)
   }
-
-  // DOM Traverse
-  // function domTraversal(){
-  //   julieTalks('inside dominos site')
-  //   var nodes = document.evaluate("//body[@id='_dpz']", document, null, XPathResult.ANY_TYPE, null)
-  //   var resultNode = nodes.iterateNext()
-  //   if (resultNode) {
-  //     alert('yo')
-  //     // Found the first node. Output its contents.
-  //     // basically outputs everything contained within the thing
-  //     // returns string
-  //     // can regex
-  //     console.log('typeof: ', typeof resultNode.innerHTML);
-  //   }
-    
-  // }
 
   // Get current tab's URL
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
@@ -43,89 +75,88 @@ setTimeout(function(){
     let shortURL = url.match(/\.(.*?)\.co/i)[1];
         julieTalks(`You're on ${shortURL}.`)
 
-  // Check if new user
-  let username;
+  fetch('http://localhost:3333/rankings/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      domainName: shortURL,
+    })
+  })
+  .then((res) => res.json())
+  .then((data)=> {
+  // have to make it modular so that it does initial get request
+  // then if the response is (sorry bro, that domain doesnt exist yet)
 
-    fetch('http://localhost:3333/rankings/', {
+  // then does web crawler algo run
+  // finds important rankings
+
+  // then does a post request 
+  // at that point, table creation + updates happen
+
+  // Check if Data is an empty array  
+  if(data.length !== 0){
+    julieTalks(`These are are the top results. ${data[0].ranking}. ${data[0].name}`)
+  } else {
+
+    alert('else statement')
+    const arrOfImportantEls = domTraversal();
+
+    fetch('http://localhost:3333/rankings/new', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        domainName: shortURL,
+        newElements: arrOfImportantEls,
       })
     })
-    .then((res) => res.json())
-    .then((data)=> {
-    // have to make it modular so that it does initial get request
-    // then if the response is (sorry bro, that domain doesnt exist yet)
+      .then(res => json(res))
+      .then((data)=> {
+      })
+    /**
+      [
+      { _id: 1, ranking: 1, dom_element: 'button', name: 'search button' }
+      ]
+    */
 
-    // then does web crawler algo run
-    // finds important rankings
+  }
 
-    // then does a post request 
-    // at that point, table creation + updates happen
 
-    // Check if Data is an empty array  
-    if(data.length !== 0){
-      julieTalks(`These are are the top results. ${data[0].ranking}. ${data[0].name}`)
-    } else {
-      domTraversal();
+    // POST req template
+    // document.querySelector('button').addEventListener('click', onclick, false)
+    
+    // function onclick(){
+    //   fetch('http://localhost:3333/rankings', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     params: JSON.stringify({
+    //     })
+    //   })
+    //     .then((res) => res.json()) 
+    // }
 
-      // fetch('http://localhost:3333/rankings/', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({
-      //     domainName: shortURL,
-      //   })
-      // })
-      //   .then(res => json(res))
-      //   .then((data)=> {
-      //   }
-      /**
-        [
-        { _id: 1, ranking: 1, dom_element: 'button', name: 'search button' }
-        ]
+    /**
+      ASSUMING THIS PART WORKS
+
+      Dom trav script is for:
+        - adding to hot keys
+        - adding to rankings
       */
 
-    }
+    // editing rankings
+
+    // creating shortcuts
+
+    // normal screen reading
 
 
-      // POST req template
-      // document.querySelector('button').addEventListener('click', onclick, false)
-     
-      // function onclick(){
-      //   fetch('http://localhost:3333/rankings', {
-      //     method: 'POST',
-      //     headers: {
-      //       'Content-Type': 'application/json'
-      //     },
-      //     params: JSON.stringify({
-      //     })
-      //   })
-      //     .then((res) => res.json()) 
-      // }
-
-      /**
-        ASSUMING THIS PART WORKS
-
-        Dom trav script is for:
-          - adding to hot keys
-          - adding to rankings
-       */
-
-      // editing rankings
-
-      // creating shortcuts
-
-      // normal screen reading
-
-
-      
-    }); // end of .then
-    }); // end of get current tab's url
+    
+  }); // end of .then
+  }); // end of get current tab's url
 });
 
 function speak (message) {
@@ -134,4 +165,4 @@ function speak (message) {
   msg.voice = voices[3]
   window.speechSynthesis.speak(msg)
 }
-speak('')
+speak('');
