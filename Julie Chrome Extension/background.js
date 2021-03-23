b1e3ea962eb731a4298da89b641bdae4180d3419
Paul Kim <khyunwoo1@gmail.com>
2021-03-22 20:56:00 -0400

    connected content.js to background.js. Event listener for all active tabs now available as well as proper fetch requests, dom traversal, and sending new dom elements to server

--- Julie Chrome Extension/background.js
+++ Julie Chrome Extension/background.js
@@ -1,1 +1,70 @@
-console.log('from background');
+function speak (message) {
+  let msg = new SpeechSynthesisUtterance(message)
+  let voices = window.speechSynthesis.getVoices()
+  msg.voice = voices[4]
+  window.speechSynthesis.speak(msg)
+}
+speak('');
+
+// TTS 
+function julieTalks(message){
+  let msg = new SpeechSynthesisUtterance(message)
+    let voices = window.speechSynthesis.getVoices()
+    msg.voice = voices[4]
+    window.speechSynthesis.speak(msg)
+}
+
+let arrOfDoms;
+// when file gets executed, we need to get a message back from content.js
+let result = chrome.runtime.onMessage.addListener((req, sender, sendRes)=>{
+  if(req.message){
+    console.log('message received');
+    console.log(req.message);
+    arrOfDoms = req.message;
+  }
+});
+
+// ! IMPORTANT: chrome.tabs.onupdate could be for refreshing and not having to actively click on tab
+chrome.tabs.onActivated.addListener(tab =>{
+
+  chrome.tabs.get(tab.tabId, current_tab_info =>{
+    let url = current_tab_info.url;
+    let shortURL = url.match(/\.(.*?)\.co/i)[1];
+    // julieTalks(shortURL)
+    fetch('http://localhost:3333/rankings/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        domainName: shortURL,
+      })
+    })
+    .then((res) => res.json())
+    .then((data)=> {
+
+      // Check if Data is an empty array  
+      if(data.length !== 0){
+        julieTalks(`These are are the top results. ${data[0].ranking}. ${data[0].name}`)
+      } else {
+        chrome.tabs.executeScript(null, {file: './content.js'}, ()=>{
+          console.log('do we have access to req.message here???', arrOfDoms)
+          fetch('http://localhost:3333/rankings/new', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              newElements: arrOfDoms.message,
+            })
+          })
+            .then(res => json(res))
+            .then((data)=> {
+            })
+          }); // end of chrome tabs execute
+      } // end of else block
+    }); // end of last .then
+  }); // end of chrome tabs get url
+});
+
+
--- Julie Chrome Extension/content.js
+++ Julie Chrome Extension/content.js
@@ -1,13 +1,13 @@
-function domTraversal(){
-
+// Algo Script 
+function domTraverse(){
   const arrOfImportantEls = [];
-  
+
   // tags obj should probably be universal
   const tags = {
     a: 'a',
     button: 'button',
   }
-  
+
   // and the unique ones will be brought to front end from backend
   const dominosHomePageParams = {
     order: 'order online',
@@ -20,7 +20,7 @@ function domTraversal(){
     SQLReference: 'SQL REFERENCE',
     learnPython: 'LEARN PYTHON',
   }
-  
+
   // Since dominos dom is protected: loop through obj of w3SchoolsHomePageParams
   // basically checks based off "type" of website, what Julie believes would be important elements
   for(let key in w3SchoolsHomePageParams){
@@ -31,10 +31,11 @@ function domTraversal(){
 
 
     // XPATH is async, document.getlementby is not.
-    alert(document.getElementsByClassName("w3-button w3-dark-grey"))
+
     // do xpath search of each critera
     // search param is for "a", but need to make dynamic also
     // CASE SENSITIVE!!!
+
     const nodes = document.evaluate(`//a[text()="${currentParam}"]`, document, null, XPathResult.ANY_TYPE, null);
     const resultNode = nodes.iterateNext();
     // should put into obj with element type and any relevant info like inner html or class/id name
@@ -43,126 +44,42 @@ function domTraversal(){
     if(resultNode){
       // Need a way to get the current result node's el/tag type and innerHTML
       const nodeText = resultNode.innerHTML;
-      alert('inside result node if ')
       importantEl.name = nodeText;
       // importantEl[dom_element] = 
       arrOfImportantEls.push(importantEl)
     } else {
-      alert('inside else of result node')
+      // alert('inside else of result node')
     }
   }
-  // returns out the array that'll be fetched
   return arrOfImportantEls;
 }
 
+// SEND DATA TO BACK END
+if(domTraverse().length !== 0){
+  console.log('inside dom traverse')
+  const myMessage = {message: domTraverse()}
+  chrome.runtime.sendMessage({message: myMessage});
 
-
-
-setTimeout(function(){ 
-
-  // TTS 
-  function julieTalks(message){
-    let msg = new SpeechSynthesisUtterance(message)
-      let voices = window.speechSynthesis.getVoices()
-      msg.voice = voices[4]
-      window.speechSynthesis.speak(msg)
-  }
-
-  // Get current tab's URL
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
-    let shortURL = url.match(/\.(.*?)\.co/i)[1];
-        julieTalks(`You're on ${shortURL}.`)
-
-  fetch('http://localhost:3333/rankings/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      domainName: shortURL,
-    })
-  })
-  .then((res) => res.json())
-  .then((data)=> {
-  // have to make it modular so that it does initial get request
-  // then if the response is (sorry bro, that domain doesnt exist yet)
-
-  // then does web crawler algo run
-  // finds important rankings
-
-  // then does a post request 
-  // at that point, table creation + updates happen
-
-  // Check if Data is an empty array  
-  if(data.length !== 0){
-    julieTalks(`These are are the top results. ${data[0].ranking}. ${data[0].name}`)
-  } else {
-
-    alert('else statement')
-    const arrOfImportantEls = domTraversal();
-
-    fetch('http://localhost:3333/rankings/new', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        newElements: arrOfImportantEls,
-      })
-    })
-      .then(res => json(res))
-      .then((data)=> {
-      })
-    /**
-      [
-      { _id: 1, ranking: 1, dom_element: 'button', name: 'search button' }
-      ]
-    */
-
-  }
-
-
-    // POST req template
-    // document.querySelector('button').addEventListener('click', onclick, false)
-    
-    // function onclick(){
-    //   fetch('http://localhost:3333/rankings', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     },
-    //     params: JSON.stringify({
-    //     })
-    //   })
-    //     .then((res) => res.json()) 
-    // }
-
-    /**
-      ASSUMING THIS PART WORKS
-
-      Dom trav script is for:
-        - adding to hot keys
-        - adding to rankings
-      */
-
-    // editing rankings
-
-    // creating shortcuts
-
-    // normal screen reading
-
-
-    
-  }); // end of .then
-  }); // end of get current tab's url
-});
-
-function speak (message) {
-  let msg = new SpeechSynthesisUtterance(message)
-  let voices = window.speechSynthesis.getVoices()
-  msg.voice = voices[3]
-  window.speechSynthesis.speak(msg)
+  // after we get a response, can we clear it?
+} else {
+  console.log('not inside dom traverse')
 }
-speak('');
+
+//     fetch('http://localhost:3333/rankings/new', {
+//       mode: 'cors',
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         newElements: result,
+//       })
+//     })
+//       .then(res => json(res))
+//       .then((data)=> {
+//       })
+//     /**
+//       [
+//       { _id: 1, ranking: 1, dom_element: 'button', name: 'search button' }
+//       ]
+//     */
